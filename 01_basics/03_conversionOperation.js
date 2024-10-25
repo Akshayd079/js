ea43ace0149310fa3a7221e581b9dc06470b3efb
Akshay Dsai <87740052+Akshayd079@users.noreply.github.com>
2024-10-25 08:15:39 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -4,3 +4,36 @@ let score = "33a"
 
 let valueInNumber = Number (score)
 // console.log (typeof valueInNumber)
+
+/// **************Operation *************
+
+let value = 3
+let negative =-value
+//console.log(negative);
+
+// console.log(2+2);
+
+let str1 = "hello"
+let str2 = " AD"
+let str3 = str1+str2;
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + 2 + "1");
+
+console.log(-true);
+
+let num1, num2, num3 
+num1 = num2 = num3 = 2 + 2
+
+console.log(num1);
+
+let gameCounter  = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
