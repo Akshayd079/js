18437f63d0e7397bc665f504c8fe19391e7a7407
Akshay Dsai <87740052+Akshayd079@users.noreply.github.com>
2024-11-11 06:28:29 +0000

    math and num

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,33 @@
+const name = "akshay"
+const repoCount = '50'
+
+// console.log(name + repoCount + "Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+const gameName = new String('Akshay-D')
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('s'));
+// console.log(gameName.toUpperCase());
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(0,1);
+// console.log(anotherString);
+
+// const newStringOne = "  Aksdhbay  "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://aklksh.com//hiotes"
+
+url.includes
+
+
+
+
