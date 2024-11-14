1be35775c3e312ca54249b2fbe4b9364575b6c3f
Akshay Dsai <87740052+Akshayd079@users.noreply.github.com>
2024-11-14 10:07:59 +0000

    json

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,18 @@
+const course = {
+    coursename : "js in hindi",
+    price: "999",
+    courseInst:"hitesh"
+}
+
+// course.courseInst
+
+const{courseInst: inst} = course
+
+console.log(inst);
+
+
+
+// const navbar = ({company}) => {
+
+// }
+// navbar(company = "hitesh")
