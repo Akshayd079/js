22ab40490593c8952545ee1003e38605794f4fe1
Akshay Dsai <87740052+Akshayd079@users.noreply.github.com>
2024-11-16 13:47:07 +0000

    function 1.0

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -33,6 +33,35 @@ function loginUserMessage(username = "sam"){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage('Akshay'));
-console.log(loginUserMessage());
+// console.log(loginUserMessage('Akshay'));
+// console.log(loginUserMessage());
 
+function calculateCartPrice(...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200,400,500));
+
+const user = {
+    username : 'Akshay',
+    price : 2000
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+// handleObject(user)
+handleObject({
+    username : 'AD',
+    price : 50
+})
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,1400,100,600]));
