c21d48433f99c57fc40ad8232c2a469e8fcd32f1
Akshay Dsai <87740052+Akshayd079@users.noreply.github.com>
2024-11-25 14:12:37 +0000

    iteration

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -6,7 +6,21 @@ const myObject = {
 }
 
 for (const key in myObject) {
-        console.log(`${key} shortcut is for ${myObject[key]}`);    
+        // console.log(`${key} shortcut is for ${myObject[key]}`);    
     }
 
-const programming = ['js']
+const programming = ['js', 'rb', 'py','java']
+for (const key in programming) {
+        // console.log(programming[key]);  
+    }
+
+//     const map = new Map()
+// map.set('IN', 'India')
+// map.set('USA', 'United States of America')
+// map.set('Fr', 'France')
+
+// for (const key in map) {
+//     console.log(key);    
+//     }
+
+
